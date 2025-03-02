3730e704bdb0122dd143aecb1a5eb648cac50b53
EugeneSarkisov <eugenesarkisov@gmail.com>
2025-03-02 17:34:17 +0300

    init commit

---
+++ src/main/java/com/local_chat/local_chat/LocalChatApplication.java
@@ -0,0 +1,13 @@
+package com.local_chat.local_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalChatApplication.class, args);
+	}
+
+}
