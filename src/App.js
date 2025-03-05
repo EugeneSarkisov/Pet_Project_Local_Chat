04f1fdec98664a60186cb221c577128b430179ec
ESKGH <evgenij.kovalev.1999@mail.ru>
2025-03-05 16:12:11 +0300

    start  commit, added react and ready to go

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './react-components/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I unga, therefore I bunga!
+        </p>
+        <p>
+          I will become beautiful...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
